aed54e908ce63778c2f5a68a8c7e6822d5c9554c
najihahzainal15 <CB23027@adab.umpsa.edu.my>
2026-05-30 02:48:44 +0800

    Update Coordinator Dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,28 +19,28 @@ class DatabaseSeeder extends Seeder
                 'matric_id' => 'CB23015', 
                 'name' => 'Project Lead', 
                 'email' => 'cb23015@student.umpsa.edu.my', 
-                'program' => 'Faculty of Computing', 
+                'program' => 'Bachelor of Computer Science (Software Engineering) with Honours', 
                 'password' => $defaultPassword
             ],
             [
                 'matric_id' => 'CB23016', 
                 'name' => 'Siti', 
                 'email' => 'siti@student.umpsa.edu.my', 
-                'program' => 'Faculty of Computing', 
+                'program' => 'Bachelor of Computer Science (Software Engineering) with Honours', 
                 'password' => $defaultPassword
             ],
             [
                 'matric_id' => 'CB23017', 
                 'name' => 'Wahidah', 
                 'email' => 'wahidah@student.umpsa.edu.my', 
-                'program' => 'Faculty of Computing', 
+                'program' => 'Bachelor of Computer Science (Software Engineering) with Honours', 
                 'password' => $defaultPassword
             ],
             [
                 'matric_id' => 'CB23018', 
                 'name' => 'Najihah', 
                 'email' => 'najihah@student.umpsa.edu.my', 
-                'program' => 'Faculty of Computing', 
+                'program' => 'Bachelor of Computer Science (Software Engineering) with Honours', 
                 'password' => $defaultPassword
             ],
         ];
@@ -52,11 +52,49 @@ class DatabaseSeeder extends Seeder
             );
         }
 
-        // 2. Generate a Dummy Lecturer Account
+        // 2. Map 6 Names Per Program 
+        $programData = [
+            'Bachelor of Computer Science (Software Engineering) with Honours' => [
+                'code' => 'CB', 'start' => 100,
+                'names' => ['Ahmad Arif', 'Chong Wei', 'Divya Nair', 'Nur Alia', 'Tan Ming', 'Kavitha']
+            ],
+            'Bachelor of Computer Science (Computer Systems & Networking) with Honours' => [
+                'code' => 'CA', 'start' => 200,
+                'names' => ['Amir Rasydan', 'Lee Jian', 'Arun Kumar', 'Siti Hajar', 'Wong Siew', 'Rohan']
+            ],
+            'Bachelor of Computer Science (Computer Graphics & Multimedia) with Honours' => [
+                'code' => 'CD', 'start' => 300,
+                'names' => ['Khairul Anwar', 'Lim Mei', 'Shanti Devi', 'Farah Hanim', 'Ng Kok', 'Suresh']
+            ],
+            'Bachelor of Computer Science (Cybersecurity) with Honours' => [
+                'code' => 'CF', 'start' => 400,
+                'names' => ['Muaz Zulkifli', 'Teoh Jin', 'Haris Rao', 'Aisyah Sofea', 'Pua Kien', 'Meena']
+            ],
+        ];
+
+        foreach ($programData as $programName => $info) {
+            foreach ($info['names'] as $index => $name) {
+                $count = $index + 1;
+                $uniqueNumber = $info['start'] + $count;
+                $matricId = $info['code'] . '23' . $uniqueNumber;
+                
+                Student::updateOrCreate(
+                    ['matric_id' => $matricId],
+                    [
+                        'name' => $name,
+                        'email' => strtolower($info['code']) . $uniqueNumber . '@student.umpsa.edu.my',
+                        'program' => $programName,
+                        'password' => $defaultPassword
+                    ]
+                );
+            }
+        }
+
+        // 3. Generate a Dummy Lecturer Account
         Lecturer::updateOrCreate(
             ['staff_id' => 'STF001'],
             [
-                'name' => 'Dr. Admin',
+                'name' => 'Dr. Fahim',
                 'email' => 'lecturer@umpsa.edu.my',
                 'research_group' => 'Software Engineering',
                 'password' => $defaultPassword
